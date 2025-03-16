c2157a834ba33a90b99334c1c02e7d4f9f97057d
Shadow <saptarshimaity1571@gmail.com>
2025-03-16 13:09:48 +0530

    config created

--- gpt-2.py
+++ gpt-2.py
@@ -2,4 +2,20 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from dataclasses import dataclass
+
+
+
+@dataclass
+class GPTConfig:
+
+    block_size: int = 12 # T -> context length to be processed 
+    vocab_size: int = 50304 # 50257 but nearest power of 2 for efficiency
+    n_layer: int = 12
+    n_head: int = 12
+    n_embd: int = 768
+    dropout: float = 0.0
+    bias: float = True
+
+
 
